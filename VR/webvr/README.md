4952a5eace63eb64a77696510393749f55a27c6c
Alex Torres <alejandro.torresmateu@adidas.com>
2018-06-09 08:56:41 +0200

    added webvr

---
+++ VR/webvr/README.md
@@ -0,0 +1,5 @@
+# adidas hackathon - webvr boilerplate
+
+```bash
+npm install && npm run start
+```
---
+++ VR/webvr/src/index.js
@@ -0,0 +1,4 @@
+require('aframe');
+require('aframe-extras');
+require('aframe-physics-system');
+require('aframe-room-component');
